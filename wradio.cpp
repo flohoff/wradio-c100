e4f120c4f7c2049f6c69b0e24a32207852982bb6
Florian Lohoff <f@zz.de>
2025-04-24 16:25:41 +0200

    Remove dead code in wradio.cpp - lots of tries

--- wradio.cpp
+++ wradio.cpp
@@ -28,27 +28,17 @@ int main(void ) {
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
 
-	//tuner.startServiceScan();
-	//std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-
-	std::shared_ptr<JDabService>	jdab;
-// Service ID 3771863228
-// Ensemble 10bc
-	//jdab=std::make_shared<JDabService>(178352000, 0xFF, 0x10bc, 3771863228);
 	// Adv PPP RTK
-	// [DabEnsemble] ServiceSanity FIC 01_05: e0d210bc : PPP-RTK-AdV
+	// Bundexmux Kanal 5C
+	// Ensemble ID 0x10bc
+	// Service ID 0xe0d210bc
+	std::shared_ptr<JDabService>	jdab;
 	jdab=std::make_shared<JDabService>(178352000, 0xFF, 0x10bc, 0xe0d210bc);
-
-	// Audio?
-	//jdab=std::make_shared<JDabService>(178352000, 0xFF, 0x10bc, 0x100d);
 
 	tuner.startService(jdab);
 
 	//tuner.startServiceScan();
-	//tuner.tuneFrequency(178352);
 
 	while(42)
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-	//wradio.run();
 }
